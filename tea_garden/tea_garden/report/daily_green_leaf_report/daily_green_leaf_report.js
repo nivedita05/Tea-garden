6de4188808a2527958d2c0ab6a56d02ba93e6777
nivedita05 <nivedita12011990@gmail.com>
2016-09-10 13:45:50 +0530

    changes made in js and py file for report

--- tea_garden/tea_garden/report/daily_green_leaf_report/daily_green_leaf_report.js
+++ tea_garden/tea_garden/report/daily_green_leaf_report/daily_green_leaf_report.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2016, frappe and contributors
 // For license information, please see license.txt
-frappe.require("assets/js/daily_green_leaf_statement.js", function() { 
+
 frappe.query_reports["Daily Green Leaf Report"] = {
 	"filters": [
 	 {
@@ -20,18 +20,39 @@ frappe.query_reports["Daily Green Leaf Report"] = {
             "options": "Estate",
             "default": frappe.defaults.get_user_default("estate_name"),
             "reqd":1
+         },
+
+        {
+            "fieldname":"bush_type",
+            "label": __("Bush"),
+            "fieldtype":"Data",
+            "options":"",
+            "default": frappe.defaults.get_user_default("Mature"),
+            "reqd":1
+         },
+
+        {
+            "fieldname":"prune_type",
+            "label": __("Prune"),
+            "fieldtype": "Link",
+            "options": "Prune Type",
+            "default": frappe.defaults.get_user_default("prune_type"),
+            "reqd":1
+         },
+
+        {
+            "fieldname":"division_name",
+            "label": __("Division"),
+            "fieldtype": "Link",
+            "options": "Division",
+            "default": frappe.defaults.get_user_default("division_name"),
+            "reqd":1
          }
 
-	],
+	]
 
 
-    "formatter": daily_green_leaf_statement.formatter,
-    "tree":true,
-    "name_fielde":"section",
-    "parent_field":"division_name",
-    "initial_depth":3
 		
     }
 
-});
 
--- tea_garden/tea_garden/report/daily_green_leaf_report/daily_green_leaf_report.py
+++ tea_garden/tea_garden/report/daily_green_leaf_report/daily_green_leaf_report.py
@@ -1,22 +1,20 @@
-# Copyright (c) 2013, frappe and contributors
-# For license information, please see license.txt
-
 from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe import utils
-
+from frappe.utils import flt
 def execute(filters=None):
 	columns = get_columns()
 	report_entries = get_report_entries(filters)
-	#frappe.throw(report_entries)
 	data = []
 	
 	
 
 	for sle in report_entries:
-		data.append([sle.section_name, sle.section_area, sle.area,sle.prune_type,sle.bush_type])
+		todate_area    = get_todate_area_pluck(sle.section_name,filters)
+		section_detail = get_section_details(sle.section_name,filters)
+		data.append([sle.section_name,section_detail[0][0],section_detail[0][1],todate_area])
 			
 	return columns, data
 
@@ -24,13 +22,17 @@ def execute(filters=None):
 	
 
 
-
+# , sle.section_area, sle.area, sle.prune_type, sle.bush_type
 
 
 def get_report_entries(filters):
-	return frappe.db.sql("""select date,section_name, section_area, area, prune_type, bush_type from `tabDaily Green Leaf in details` where estate_name = %s and date= %s""",(filters.estate_name,filters.date),as_dict=1)
+	return frappe.db.sql("""select distinct section_name from `tabDaily Green Leaf in details` where estate_name = %s and prune_type=%s and bush_type=%s and division_name=%s and date BETWEEN %s AND %s ORDER BY date DESC""",(filters.estate_name,filters.prune_type,filters.bush_type,filters.division_name,'2016-01-01', filters.date),as_dict=1)
 
+def get_todate_area_pluck(section_name,filters):
+	return frappe.db.sql("""select sum(area) from `tabDaily Green Leaf in details` where section_name = %s and date BETWEEN %s AND %s ORDER BY date DESC LIMIT 1 """,(section_name, '2016-01-01',filters.date))
 
+def get_section_details(section_name,filters):
+	return frappe.db.sql("""select section_area,area from `tabDaily Green Leaf in details` where section_name = %s ORDER BY date DESC LIMIT 1""",(section_name)) 
 
 def get_sle_conditions(filters):
 	conditions = []
@@ -46,6 +48,7 @@ def get_columns():
 			"fieldtype": "Link",
 			"options": "daily_green_leaf_in_details",
 			"width": 120
+
 		}]
 		
 		columns.append({
@@ -68,28 +71,11 @@ def get_columns():
 
 
 		columns.append({
-			"fieldname": "prune_type",
-			"label": _("Prune Type"),
-			"fieldtype":"Link",
-			"options":"daily_green_leaf_in_details",
-			"width":133
-		})
-
-
-		columns.append({
-			"fieldname": "bush_type",
-			"label": _("Bush Type"),
-			"fieldtype": "Link",
-			"options":"daily_green_leaf_in_details",
+			"label": _("To Date"),
+			"fieldtype": "Float",
 			"width":135
 		})
-
-
 		
 		return columns
 
 	
-	
-   
-
-
